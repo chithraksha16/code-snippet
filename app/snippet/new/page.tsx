8eb991c4e33766341b83ca45ab19f6a44e1d6f32
chithraksha16 <chithrakshakharvi@gmail.com>
2026-05-11 19:03:15 +0530

    added prisma-client and createsnippet function

--- app/snippet/new/page.tsx
+++ app/snippet/new/page.tsx
@@ -1,17 +1,39 @@
-
 import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
+import { prisma } from "@/lib/prisma"
+import { redirect } from "next/navigation"
 const newSnippet = () => {
-return (
-    <div className="px-20 space-y-4 mt-5">
-    <div>
-        <Label>Title</Label>
-<input name="title" id="title" />
-    </div>
-<div>
-<Label>Code</Label>
-</div>
-    </div>
-)
+    const createSnippet=async(formData:FormData)=>{
+        const title=formData.get("title");
+        const code =formData.get("code");
+
+        const snippet=await prisma.snippet.create({
+            data:{
+                title,
+                code
+            }
+        });
+        console.log("Snippet:",snippet)
+        redirect("/")
+
+    }
+    return (
+        <div className="px-20 space-y-4 mt-5">
+            <form action={createSnippet}>
+            <div>
+                <Label>Title</Label>
+                <Input type="text" name="title" id="title" />
+            </div>
+            <div>
+                <Label>Code</Label>
+                <Textarea name="code" id="code" />
+            </div>
+            <Button>New</Button>
+            </form>
+        </div>
+    )
 }
 
 export default newSnippet
