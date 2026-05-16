45d339eea1e139f8a3102a98f3aa590ad1b1e357
chithraksha16 <chithrakshakharvi@gmail.com>
2026-05-16 17:22:07 +0530

    updated editSnippet form

--- components/EditSnippet.tsx
+++ components/EditSnippet.tsx
@@ -3,18 +3,26 @@
 import { Editor } from "@monaco-editor/react"
 import { Snippet } from "@prisma/client"
 import { useState } from "react"
-const EditSnippet = ({snippet}:{snippet:Snippet}) => {
-    const [code,setCode]=useState();
-  return (
-    <div>
-      <Editor
-        height="40vh"
-        defaultLanguage="javascript"
-        defaultValue={code}
-        
-      />
-    </div>
-  )
+import { Button } from "./ui/button"
+const EditSnippet = ({ snippet }: { snippet: Snippet }) => {
+    const [code, setCode] = useState(snippet.code);
+    return (
+        <div className="flex flex-col gap-4 px-20 space-y-4 mt-5">
+            <form
+            className="flex items-center justify-between"
+            action="">
+                <h1 className="font-bold text-xl">Your code Editor</h1>
+                <Button>Save</Button>
+            </form>
+            <Editor
+                height="40vh"
+                theme="vs-dark"
+                defaultLanguage="javascript"
+                defaultValue={code}
+
+            />
+        </div>
+    )
 }
 
 export default EditSnippet
