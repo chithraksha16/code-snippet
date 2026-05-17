969a12b89e4c973ff097c045c772229269dfd18f
chithraksha16 <chithrakshakharvi@gmail.com>
2026-05-17 21:21:28 +0530

    updated EditSnippet and added server component

---
+++ actions/index.ts
@@ -0,0 +1,19 @@
+"use server"
+
+import prisma from "@/lib/prisma"
+import { redirect } from "next/navigation";
+
+
+ const saveSnippet=async(id:number,code:string)=>{
+    await prisma.snippet.update({
+        where:{
+            id
+        },
+        data:{
+            code
+        }
+    });
+    redirect('/')
+}
+
+export default saveSnippet
--- components/EditSnippet.tsx
+++ components/EditSnippet.tsx
@@ -4,15 +4,17 @@ import { Editor } from "@monaco-editor/react"
 import { Snippet } from "@prisma/client"
 import { useState } from "react"
 import { Button } from "./ui/button"
+import saveSnippet  from "@/actions"
 const EditSnippet = ({ snippet }: { snippet: Snippet }) => {
     const [code, setCode] = useState(snippet.code);
+    const saveSnippetAction=saveSnippet.bind(null,snippet.id,code)
     return (
         <div className="flex flex-col gap-4 px-20 space-y-4 mt-5">
             <form
             className="flex items-center justify-between"
-            action="">
+            action={saveSnippetAction}>
                 <h1 className="font-bold text-xl">Your code Editor</h1>
-                <Button>Save</Button>
+                <Button type="submit">Save</Button>
             </form>
             <Editor
                 height="40vh"
