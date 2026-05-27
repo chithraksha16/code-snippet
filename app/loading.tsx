781d4904f21dfc07d9c99ba7086c2234f13c1d48
chithraksha16 <chithrakshakharvi@gmail.com>
2026-05-27 18:06:37 +0530

    added loading file

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+
+
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <span>Loading....</span>
+    </div>
+  )
+}
+
+export default loading
